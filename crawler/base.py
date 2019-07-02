2335591f86eae19808d0668f8f117bf8b35eaf00
Fabian Ludwig <info@fabianludwig.com>
2019-07-02 02:57:49 +0200

    tidied up ModelCrawler

--- crawler/base.py
+++ crawler/base.py
@@ -16,11 +16,13 @@ class ModelCrawler():
 	filename		= 'models'
 
 	_errors			= []
+	_path			= None
+	_temp_path		= None
 
 	def __init__(self):
-		self.set_browser()
+		self._path = self.download_folder+'/'+self.filename+'.csv'
+		self._temp_path = self.download_folder+'/.'+self.filename+'_temp.csv'
 
-	def set_browser(self):
 		options = webdriver.ChromeOptions()
 		if headless_browser:
 			options.add_argument('headless')
@@ -31,17 +33,15 @@ class ModelCrawler():
 		crawled_urls = self._get_crawled_urls()
 
 		# Delete old file
-		if os.path.isfile(self.download_folder+'/'+self.filename+'.csv') and len(crawled_urls) == 0:
-			os.remove(self.download_folder+'/'+self.filename+'.csv')
+		if os.path.isfile(self._path) and len(crawled_urls) == 0:
+			os.remove(self._path)
 
-		self.browser.get(self.base_url)
-		items = self.browser.find_elements_by_css_selector(self.link_selector)
-		item_urls = []
-
-		for item in items:
-			item_urls.append(item.get_attribute('href'))
+		item_urls = self._get_urls()
 		
-		print("{} items to scrape! ({} already done!)".format(len(item_urls), len(crawled_urls)))
+		print("{} items to scrape! ({} already done!)".format(
+			len(item_urls), 
+			len(crawled_urls),
+		))
 
 		counter = 0
 		for item_url in item_urls:
@@ -51,34 +51,53 @@ class ModelCrawler():
 				try:
 					item = self._scrape_item()
 
-					with open(self.download_folder+'/'+self.filename+'.csv', 'a') as file:
+					with open(self._path, 'a') as file:
 						writer = csv.writer(file)
 						writer.writerow(item.values())
 					
-					with open(self.download_folder+'/.'+self.filename+'_temp.csv', 'a') as file:
+					with open(self._temp_path, 'a') as file:
 						writer = csv.writer(file)
 						writer.writerow([item_url])
 					
-					print("({}/{}) {}".format(counter, len(item_urls), item))
+					print("({}/{}) {}".format(
+						counter, 
+						len(item_urls), 
+						item,
+					))
 				except:
 					self._errors.append(item_url)
-					print("{} resulted in an error!".format(item_url), sys.exc_info()[1])
+					print("{} resulted in an error! ({})".format(
+						item_url, 
+						sys.exc_info()[1],
+					))
 		
-		self.clean_up()
+		self._clean_up()
 	
+	def _get_urls(self):
+		self.browser.get(self.base_url)
+
+		items = self.browser.find_elements_by_css_selector(self.link_selector)
+		item_urls = []
+		for item in items:
+			item_urls.append(
+				item.get_attribute('href')
+			)
+		
+		return item_urls
+
 	def _get_crawled_urls(self):
-		if os.path.isfile(self.download_folder+'/.'+self.filename+'_temp.csv'):
-			with open(self.download_folder+'/.'+self.filename+'_temp.csv', 'r') as file:
+		if os.path.isfile(self._temp_path):
+			with open(self._temp_path, 'r') as file:
 				reader = csv.reader(file)
 				your_list = list(reader)
 				return [url[0] for url in your_list]
 		else:
 			return []
 
-	def clean_up(self):
+	def _clean_up(self):
 		if len(self._errors) == 0:
-			if os.path.isfile(self.download_folder+'/.'+self.filename+'_temp.csv'):
-				os.remove(self.download_folder+'/.'+self.filename+'_temp.csv')
+			if os.path.isfile(self._temp_path):
+				os.remove(self._temp_path)
 		
 	def _scrape_item(self):
-		return {}
+		return ({})
